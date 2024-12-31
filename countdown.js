c12f56499c823094d8c2881d4d588ff57bfc8c8e
YellDevop <lucaiannello927@gmail.com>
2024-12-31 12:01:21 +0100

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,5 +1,5 @@
 // Data target del countdown (ad esempio, 1 gennaio 2025)
-const targetDate = new Date("May 17, 2025 17:00:00").getTime();
+const targetDate = new Date("May 17, 2025 17:00:00 UTC").getTime();
 
 // Funzione per aggiornare il countdown
 function updateCountdown() {
